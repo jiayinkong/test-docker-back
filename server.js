ab6783b42c4fec295ada5ad207f2e63b88f83fba
jiayinkong <ry1n.kong@gmail.com>
2023-11-09 23:47:05 +0800

    feat: create a http server

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const users = [
+  {
+    id: 1,
+    name: 'Tom',
+  },
+  {
+    id: 2,
+    name: 'Jerry'
+  }
+]
+
+const server = http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if(req.url === '/api/users') {
+    res.end(
+      JSON.stringify(users)
+    )
+  }
+})
+
+server.listen(3000, () => {
+  console.log('server is starting at port 3000!!')
+})
